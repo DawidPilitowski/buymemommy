3b333d096c37a04ee328adb380ddc71240e1603f
Dawid <d.pilitowski@gmail.com>
2018-08-14 20:58:22 +0200

    Add usr to database

--- src/main/java/pl/sda/buymemommy/configuration/BasicSecurity.java
+++ src/main/java/pl/sda/buymemommy/configuration/BasicSecurity.java
@@ -9,11 +9,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import pl.sda.buymemommy.service.AppUserDetailsService;
 
 @Configuration
 public class BasicSecurity {
 
     //TODO podwiązać servis do kodowania hasła!!
+    @Autowired
+    private AppUserDetailsService appUserDetailsService;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -22,41 +25,43 @@ public class BasicSecurity {
 
     //TODO do sprawdzenia i zmienienia
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(
-                        "/",
-                        "/about",
-                        "/register",
-                        "/webjars/**",
-                        "/css/**").permitAll()
-                .anyRequest()
-                .authenticated()
-//                .antMatchers(HttpMethod.POST, "/admin/**").hasRole("ADMIN")
-                .and().formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/")
-                .permitAll()
-                .and().logout()
-                .clearAuthentication(true)
-                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true);
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers(
+//                        "/",
+//                        "/about",
+//                        "/register",
+//                        "/webjars/**",
+//                        "/css/**").permitAll()
+//                .anyRequest()
+//                .authenticated()
+////                .antMatchers(HttpMethod.POST, "/admin/**").hasRole("ADMIN")
+//                .and().formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/")
+//                .permitAll()
+//                .and().logout()
+//                .clearAuthentication(true)
+//                .logoutUrl("/logout")
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/")
+//                .invalidateHttpSession(true);
+//    }
 
     //TODO podwiązać servis !!
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(){
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider =
                 new DaoAuthenticationProvider();
         daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
         daoAuthenticationProvider.setUserDetailsService(appUserDetailsService);
+        return daoAuthenticationProvider;
     }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
 }
--- src/main/java/pl/sda/buymemommy/service/AppUserDetailsService.java
+++ src/main/java/pl/sda/buymemommy/service/AppUserDetailsService.java
@@ -1,4 +1,22 @@
 package pl.sda.buymemommy.service;
 
-public class AppUserDetailsService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.sda.buymemommy.repository.AppUserRepository;
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bcryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
