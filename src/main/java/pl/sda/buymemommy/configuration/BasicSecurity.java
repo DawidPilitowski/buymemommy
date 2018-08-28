b06c46319c623e4afa7efe02304e0a874d6819ae
marcinb <kllz.barkas@gmail.com>
2018-08-28 13:28:16 +0200

    - login/register page footers removed - basic security enabled

--- src/main/java/pl/sda/buymemommy/configuration/BasicSecurity.java
+++ src/main/java/pl/sda/buymemommy/configuration/BasicSecurity.java
@@ -4,17 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import pl.sda.buymemommy.service.AppUserDetailsService;
 
 @Configuration
-public class BasicSecurity {
-
-    //TODO podwiązać servis do kodowania hasła!!
+public class BasicSecurity extends WebSecurityConfigurerAdapter {
     @Autowired
     private AppUserDetailsService appUserDetailsService;
 
@@ -23,45 +21,58 @@ public class BasicSecurity {
         return new BCryptPasswordEncoder();
     }
 
-    //TODO do sprawdzenia i zmienienia
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and().formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and().logout()
+                .clearAuthentication(true)
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);
+    }
 
+// TODO : Zmienic konfiguracje
 //    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers(
-//                        "/",
-//                        "/about",
-//                        "/register",
-//                        "/webjars/**",
-//                        "/css/**").permitAll()
-//                .anyRequest()
-//                .authenticated()
-////                .antMatchers(HttpMethod.POST, "/admin/**").hasRole("ADMIN")
-//                .and().formLogin()
-//                .loginPage("/login")
-//                .defaultSuccessUrl("/")
-//                .permitAll()
-//                .and().logout()
-//                .clearAuthentication(true)
-//                .logoutUrl("/logout")
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/")
-//                .invalidateHttpSession(true);
-//    }
-
-    //TODO podwiązać servis !!
+////    protected void configure(HttpSecurity http) throws Exception {
+////        http.authorizeRequests()
+////                .antMatchers(
+////                        "/",
+////                        "/about",
+////                        "/register",
+////                        "/webjars/**",
+////                        "/css/**").permitAll()
+////                .anyRequest()
+////                .authenticated()
+//////                .antMatchers(HttpMethod.POST, "/admin/**").hasRole("ADMIN")
+////                .and().formLogin()
+////                .loginPage("/login")
+////                .defaultSuccessUrl("/")
+////                .permitAll()
+////                .and().logout()
+////                .clearAuthentication(true)
+////                .logoutUrl("/logout")
+////                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+////                .logoutSuccessUrl("/")
+////                .invalidateHttpSession(true);
+////    }
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider =
-                new DaoAuthenticationProvider();
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
         daoAuthenticationProvider.setUserDetailsService(appUserDetailsService);
         return daoAuthenticationProvider;
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(daoAuthenticationProvider());
-//    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
 }
